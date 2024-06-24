0c4c625f0ff5c283055082d70e7f41e1252b4199
450245869 <35650168+450245869@users.noreply.github.com>
2024-06-24 15:29:28 +0800

    Delete assets directory

--- assets/wxpay.Bc05EOdG.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/alipay-BAm7Ynxu.png",a="/assets/wxpay-BdwhbpCt.png";export{s as _,a};
--- assets/wxpay.DqcVInL5.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/alipay-4rIud8xj.png",a="/assets/wxpay-B85sseEO.png";export{s as _,a};
